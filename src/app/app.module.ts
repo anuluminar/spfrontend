ea03ce9d72b60e4eca6afeb57bd1fde45c8f90f3
anuluminar <anuluminar@gmail.com>
2024-02-27 10:05:15 +0530

    studentportal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { StudentAddComponent } from './student-add/student-add.component';
+import { StudentListComponent } from './student-list/student-list.component';
+import { StudentEditComponent } from './student-edit/student-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    DashboardComponent,
+    StudentAddComponent,
+    StudentListComponent,
+    StudentEditComponent
   ],
   imports: [
     BrowserModule,
